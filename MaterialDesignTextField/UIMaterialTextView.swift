d26e3ed20b9778c6ff6a3838adef60d8af685daa
Kaan Emec <kaanemec@gmail.com>
2015-03-30 23:09:58 +0300

    Working as a class, no documentation

---
+++ MaterialDesignTextField/UIMaterialTextView.swift
@@ -0,0 +1,180 @@
+//
+//  UIMaterialTextView.swift
+//  MaterialDesignTextField
+//
+//  Created by Kaan Emeç on 30.03.2015.
+//  Copyright (c) 2015 KE. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol UIMaterialTextViewDelegate : NSObjectProtocol {
+
+    optional func materialTextFieldShouldBeginEditing(textField: UITextField) -> Bool // return NO to disallow editing.
+    optional func materialTextFieldDidBeginEditing(textField: UITextField) // became first responder
+    optional func materialTextFieldShouldEndEditing(textField: UITextField) -> Bool // return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+    optional func materialTextFieldDidEndEditing(textField: UITextField) // may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+    optional func materialTextField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool // return NO to not change text
+    
+    optional func materialTextFieldShouldClear(textField: UITextField) -> Bool // called when clear button pressed. return NO to ignore (no notifications)
+    optional func materialTextFieldShouldReturn(textField: UITextField) -> Bool // called when 'return' key pressed. return NO to ignore.
+}
+
+
+class UIMaterialTextView: UITextField, UITextFieldDelegate {
+
+    var materialDelegate: UIMaterialTextViewDelegate!
+    private var PlaceHolderText: UILabel!
+    private var TitleText: UILabel!
+    private var placeHolderString: String = ""
+    var animDurationFocus: NSTimeInterval = 0.2
+    var animDurationLostFocus: NSTimeInterval = 0.4
+    var animDurationEditing: NSTimeInterval = 0.3
+    var placeHolderColor: UIColor = UIColor.grayColor()
+    var activeTitleColor: UIColor = UIColor.blueColor()
+    var inactiveTitleColor: UIColor = UIColor.grayColor()
+    var initialPlaceHolderAlpha: CGFloat = 0.5
+
+    required init(coder aDecoder: NSCoder?) {
+        super.init(coder: aDecoder!)
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.delegate = self
+        self.awakeFromNib()
+    }
+    
+    override var placeholder: String? {
+        get {
+            return self.placeHolderString
+        }
+        set {
+            if self.placeHolderString == "" {
+                self.placeHolderString = newValue!
+                PlaceHolderText = UILabel(frame: CGRect(x: padding.left, y: padding.top + 10, width: self.frame.width, height: self.frame.height-padding.top))
+                PlaceHolderText.text = self.placeHolderString
+                PlaceHolderText.font = self.font
+                PlaceHolderText.textColor = self.placeHolderColor
+                PlaceHolderText.alpha = self.initialPlaceHolderAlpha
+                PlaceHolderText.sizeToFit()
+                
+                TitleText = UILabel(frame: CGRect(x: padding.left, y:14, width: self.frame.width, height: self.frame.height))
+                TitleText.text = self.placeHolderString
+                TitleText.font = UIFont(name: PlaceHolderText.font.familyName, size: 10)
+                TitleText.textColor = self.inactiveTitleColor
+                TitleText.sizeToFit()
+                TitleText.alpha = 0.0
+                
+                self.addSubview(PlaceHolderText)
+                self.addSubview(TitleText)
+            } else {
+                self.placeHolderString = newValue!
+                PlaceHolderText.text = self.placeHolderString
+                PlaceHolderText.sizeToFit()
+                TitleText.text = self.placeHolderString
+                TitleText.sizeToFit()
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        self.borderStyle = UITextBorderStyle.None
+        self.layer.borderWidth = 0
+        
+        var Line = UIView(frame: CGRect(x: padding.left/2, y: self.frame.height-2, width: self.frame.width, height: 1))
+        Line.backgroundColor = UIColor.grayColor()
+        
+        self.addSubview(Line)
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if textField.text == "" && countElements(string)>0 {
+            TitleText.textColor = self.activeTitleColor
+            UIView.animateWithDuration(animDurationEditing - 0.1, animations: { () -> Void in
+                self.PlaceHolderText.alpha = 0.0
+            }, completion: nil)
+                    
+            UIView.animateWithDuration(animDurationEditing, animations: { () -> Void in
+                self.TitleText.alpha = 1
+                self.TitleText.frame = CGRect(x: self.padding.left, y:-14, width: textField.frame.width, height: textField.frame.height)
+            }, completion: nil)
+        }
+        
+        return materialDelegate.materialTextField?(textField, shouldChangeCharactersInRange: range, replacementString: string) ?? true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if textField.text == "" {
+                UIView.animateWithDuration(animDurationFocus, animations: { () -> Void in
+                    self.PlaceHolderText.alpha = 1
+                    }, completion: nil)
+        } else {
+                UIView.transitionWithView(self.TitleText, duration: animDurationFocus, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
+                        self.TitleText.textColor = self.activeTitleColor
+                }, completion: nil)
+        }
+        materialDelegate.materialTextFieldDidBeginEditing?(textField)
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField) {
+        if textField.text != "" {
+            UIView.transitionWithView(self.TitleText, duration: animDurationLostFocus - 0.1, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
+                    self.TitleText.textColor = self.inactiveTitleColor
+            }, completion: nil)
+        } else {
+            UIView.animateWithDuration(animDurationLostFocus, animations: { () -> Void in
+                self.PlaceHolderText.alpha = self.initialPlaceHolderAlpha
+            }, completion: nil)
+                    
+            UIView.animateWithDuration(animDurationLostFocus, animations: { () -> Void in
+                self.TitleText.alpha = 0
+                self.TitleText.frame = CGRect(x: self.padding.left, y:-5, width: textField.frame.width, height: textField.frame.height)
+            }, completion: {(completed) -> Void in
+                self.TitleText.frame = CGRect(x: self.padding.left, y:14, width: textField.frame.width, height: textField.frame.height)
+            })
+        }
+        
+        materialDelegate.materialTextFieldDidEndEditing?(textField)
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        return materialDelegate.materialTextFieldShouldReturn?(textField) ?? true // called when 'return' key pressed. return NO to ignore.
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return materialDelegate.materialTextFieldShouldBeginEditing?(textField) ?? true // called when 'return' key pressed. return NO to ignore.
+    }
+
+    func textFieldShouldEndEditing(textField: UITextField) -> Bool {
+        return materialDelegate.materialTextFieldShouldEndEditing?(textField) ?? true
+    }
+
+    func textFieldShouldClear(textField: UITextField) -> Bool {
+        return materialDelegate.materialTextFieldShouldClear?(textField) ?? true
+    }
+    
+    
+    private let padding = UIEdgeInsets(top: 5, left: 5, bottom: 0, right: 0);
+    
+    override func textRectForBounds(bounds: CGRect) -> CGRect {
+        return self.newBounds(bounds)
+    }
+    
+    override func placeholderRectForBounds(bounds: CGRect) -> CGRect {
+        return self.newBounds(bounds)
+    }
+    
+    override func editingRectForBounds(bounds: CGRect) -> CGRect {
+        return self.newBounds(bounds)
+    }
+    
+    private func newBounds(bounds: CGRect) -> CGRect {
+        var newBounds = bounds
+        newBounds.origin.x += padding.left
+        newBounds.origin.y += padding.top
+        return newBounds
+    }
+}
--- MaterialDesignTextField/ViewController.swift
+++ MaterialDesignTextField/ViewController.swift
@@ -8,92 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController, UITextFieldDelegate, UIMaterialTextViewDelegate {
 
-
+    @IBOutlet var plp: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+                
         // Do any additional setup after loading the view, typically from a nib.
-        var myTextField = UITextField(frame: CGRect(x: 20, y: 120, width: 300, height: 46))
-        myTextField.placeholder = "Title"
-        myTextField.borderStyle = UITextBorderStyle.None
-        myTextField.layer.borderWidth = 0
+        var myTextField = UIMaterialTextView(frame: CGRect(x: 20, y: 120, width: 300, height: 40))
+        myTextField.materialDelegate = self
+        myTextField.activeTitleColor = UIColor.purpleColor()
+        myTextField.initialPlaceHolderAlpha = 0.2
+        myTextField.placeholder = "Artist"
         self.view.addSubview(myTextField)
-        myTextField.delegate = self
-        
-        var Line = UIView(frame: CGRect(x: 0, y: myTextField.frame.height-2, width: myTextField.frame.width, height: 1))
-        Line.backgroundColor = UIColor.grayColor()
-        myTextField.addSubview(Line)
-        
-        var placeHolderString = myTextField.placeholder
-        myTextField.placeholder = ""
-        
-        var PlaceHolderText = UILabel(frame: CGRect(x: padding.left, y: padding.top + 8, width: myTextField.frame.width, height: myTextField.frame.height-padding.top))
-        PlaceHolderText.text = placeHolderString
-        PlaceHolderText.font = myTextField.font
-        PlaceHolderText.textColor = UIColor.grayColor()
-        PlaceHolderText.sizeToFit()
-        
-        myTextField.addSubview(PlaceHolderText)
         
     }
     
+    func materialTextFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return true
+    }// return NO to disallow editing.
     
-    func textFieldShouldReturn(textField: UITextField) -> Bool {
+    func materialTextFieldDidBeginEditing(textField: UITextField) {
+        
+    }// became first responder
+
+    func materialTextFieldShouldEndEditing(textField: UITextField) -> Bool {
+        return true
+    }// return YES to allow editing to stop and to resign first responder status. NO to disallow the editing session to end
+    
+    func materialTextFieldDidEndEditing(textField: UITextField) {
+        
+    }// may be called if forced even if shouldEndEditing returns NO (e.g. view removed from window) or endEditing:YES called
+    
+    func materialTextField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }// return NO to not change text
+    
+    func materialTextFieldShouldClear(textField: UITextField) -> Bool {
+        return true
+    }// called when clear button pressed. return NO to ignore (no notifications)
+    
+    func materialTextFieldShouldReturn(textField: UITextField) -> Bool {
         textField.endEditing(true)
         return false
-    }
-    
-    func textFieldDidBeginEditing(textField: UITextField) {
-        if textField.text == "" {
-        if let placeHolder = textField.subviews[1] as? UILabel {
-            UIView.animateWithDuration(0.3, animations: { () -> Void in
-                placeHolder.transform = CGAffineTransformScale(placeHolder.transform, 0.8, 0.8)
-                placeHolder.frame = CGRect(x: padding.left, y: 2, width: placeHolder.frame.width, height: placeHolder.frame.height)
-                }, completion: { (completed) -> Void in
-                if completed {
-                    UIView.transitionWithView(placeHolder, duration: 3, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
-                        placeHolder.textColor = UIColor.blueColor()
-                        }, completion: nil)
-                }
-            })
-            }}
-    }
-    
-    func textFieldDidEndEditing(textField: UITextField) {
-        if textField.text.stringByTrimmingCharactersInSet(NSCharacterSet.whitespaceAndNewlineCharacterSet()) == "" {
-        if let placeHolder = textField.subviews[1] as? UILabel {
-            UIView.transitionWithView(placeHolder, duration: 0.2, options: UIViewAnimationOptions.TransitionCrossDissolve, animations: { () -> Void in
-                    placeHolder.textColor = UIColor.grayColor()
-                }, completion: { (completed) -> Void in
-                        if completed {
-                            UIView.animateWithDuration(0.2, animations: { () -> Void in
-                            placeHolder.transform = CGAffineTransformScale(placeHolder.transform, 1.25, 1.25)
-                            placeHolder.frame = CGRect(x: padding.left, y: padding.top + 8, width: placeHolder.frame.width, height: placeHolder.frame.height)
-                            })
-                    }
-            })
-            }
-        }
-    }
-    
-    
-    
-    func textViewDidBeginEditing(textView: UITextView) {
-        
-        //UIView.animateWithDuration(1.0, animations: { () -> Void in
-        //    if let plc = self.myTextField.subviews[0] as? UILabel {
-        //        plc.textColor = UIColor.whiteColor()
-        //    }
-        //    })
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    }// called when 'return' key pressed. return NO to ignore.
 }
 
